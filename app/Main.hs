db7bccb7984ff58169366c2711802a79a2af3ea9
Christophe Thibaut <cthibaut@octo.com>
2022-10-12 23:03:00 +0200

    first app

---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
+
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
+
+
+main :: IO ()
+main = do
+    Gtk.init Nothing
+    win <- new Gtk.Window [#title := "Introduction"]
+    on win #destroy Gtk.mainQuit
+    #resize win 640 480
+    box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+    #add win box
+    msg <- new Gtk.Label [#label := "Hello"]
+    #add box msg
+    btn <- new Gtk.Button [#label := "Click me!"]
+    #add box btn
+    #packStart box btn False False 10
+    on btn #clicked (set msg [#label := "Clicked!"])
+    #showAll win
+    Gtk.main
